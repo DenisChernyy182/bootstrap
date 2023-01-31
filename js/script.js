9e0f0fbfcc9cdb42a0c1729d1f17402e5076bdab
DenisChernyy182 <chernyasha94@mail.ru>
2023-01-31 21:31:39 +0300

    dev

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelector(".burger").addEventListener("click", function() {
+
+      document.querySelector(".header").classList.toggle("is-active")
+
+    });    
+    
+  });
+
